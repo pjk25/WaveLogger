d235fbfafe2abcf7af37383bfe5c4dfb30b0bb97
Philip Kuryloski <pjk25@cornell.edu>
2011-07-05 18:43:26 -0700

    Added missing Cursor.close() call

--- src/edu/berkeley/androidwave/wavelogger/DbHelper.java
+++ src/edu/berkeley/androidwave/wavelogger/DbHelper.java
@@ -123,6 +123,7 @@ public class DbHelper {
                         c.moveToNext();
                     }
                 }
+                c.close();
             }
             finally {
                 out.close();
